f154e29bd06eb5f85ff22be1b46ea91df6e65c01
DmitryMix260 <dmitry-buyanov1995@yandex.ru>
2022-09-13 21:12:53 +0300

    Add test for addMessage

--- src/components/message/AddMessage.test.jsx
+++ src/components/message/AddMessage.test.jsx
@@ -1,11 +1,43 @@
-import { render, screen } from '@testing-library/react';
 import { AddMessage } from './AddMessage';
+import { fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe(AddMessage, () => {
-  it('render with snapshot', () => {
-    const { asFragment } = render(<AddMessage disabled={false} />);
+  let addMessage, component, input, button;
+  beforeEach(() => {
+    addMessage = jest.fn();
+    component = render(<AddMessage addMessage={addMessage} />);
+    input = component.getByRole('input');
+    button = component.getByRole('button');
+  });
+  it('render components', () => {
+    render(<AddMessage />);
+  });
 
-    expect(asFragment()).toMatchSnapshot();
+  it('input change with fireevent', () => {
+    fireEvent.change(input, {
+      target: { value: 'message' },
+    });
+    expect(input.value).toBe('message');
+  });
+
+  it('button disable with fireevent.', () => {
+    expect(button.disabled).toBeFalsy;
+  });
+
+  it('button change disable with fireevent, expect true', () => {
+    fireEvent.change(button, {
+      target: { disabled: true },
+    });
+    expect(button.disabled).toBeTruthy;
+  });
+
+  it('form submit with fireEvent', () => {
+    fireEvent.change(input, { target: { value: 'new value' } });
+    expect(input.value).toBe('new value');
+
+    fireEvent.submit(component.getByTestId('addmessage'));
+    fireEvent.click(button);
+    expect(addMessage).toHaveBeenCalledTimes(1);
   });
 });
